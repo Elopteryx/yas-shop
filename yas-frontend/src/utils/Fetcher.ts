bb35e184891ace89098b91f203acc38195105b56
Adam Forgacs <adamforgacs256@gmail.com>
2019-05-24 21:37:15 +0200

    Updated the Fetcher and one dependency.

--- yas-frontend/src/utils/Fetcher.ts
+++ yas-frontend/src/utils/Fetcher.ts
@@ -1,32 +1,37 @@
 import Delay from "./Delay";
+import Timeout from "./Timeout";
 
 const Fetcher = {
     GET<T extends object | string>(url: string): Promise<T> {
-        return Promise.all([fetch(url, {
-            method: "GET",
-            mode: "no-cors",
-            cache: "no-cache",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            redirect: "follow",
-            referrer: "no-referrer",
-        }), Delay.by(500, undefined)]).then(([response]) => response.json());
+        return Promise.race(
+            [Promise.all([fetch(url, {
+                method: "GET",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+                redirect: "follow",
+                referrer: "no-referrer",
+            }), Delay.by(500, undefined)]).then(([response]) => response.json()),
+                Timeout.in(20000)]);
     },
     POST<T>(url: string, body: object): Promise<T> {
-        return Promise.all([fetch(url, {
-            method: "POST",
-            mode: "no-cors",
-            cache: "no-cache",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            redirect: "follow",
-            referrer: "no-referrer",
-            body: JSON.stringify(body),
-        }), Delay.by(500, undefined)]).then(([response]) => response.json());
+        return Promise.race(
+            [Promise.all([fetch(url, {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+                redirect: "follow",
+                referrer: "no-referrer",
+                body: JSON.stringify(body),
+            }), Delay.by(500, undefined)]).then(([response]) => response.json()),
+                Timeout.in(20000)]);
     }
 };
 
