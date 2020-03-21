dade682dbf71d5d1bcc49c6199ee32e0e6426150
Adam Forgacs <adamforgacs256@gmail.com>
2020-03-21 08:30:54 +0100

    Fixed backend compile.

--- yas-backend/src/main.rs
+++ yas-backend/src/main.rs
@@ -11,13 +11,12 @@ use crate::version::version;
 use crate::store::{item_all, item_by_id};
 use crate::sorting::{sort_with_insertion, sort_with_merge, sort_with_quick};
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/user", web::get().to(user_current))
             //.route("/delay", web::get().to(delay))
             .route("/version", web::get().to(version))
